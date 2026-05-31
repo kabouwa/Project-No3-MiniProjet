afd3c55b400e31bf8b9a5efb8355a159259ed976
Kabouwa <med.rh.med27@gmail.com>
2026-05-31 13:24:21 +0100

    ReFix Head

--- assets/js/parts.js
+++ assets/js/parts.js
@@ -1,7 +1,6 @@
 //<a href="/pages/dashboard.html"><img src="" alt="logo" id="logo"></a>
 const head = `
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+www
 <link rel="icon" href="https://cdn-icons-png.flaticon.com/512/33/33308.png">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
